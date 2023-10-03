41027c2f366bb37250d9bc969b557718250d555e
Jose Castro <josediogonc@gmail.com>
2023-10-03 15:01:39 -0300

    test code

--- app/src/main/java/com/beon/androidchallenge/ui/main/MainFragment.kt
+++ app/src/main/java/com/beon/androidchallenge/ui/main/MainFragment.kt
@@ -1,10 +1,12 @@
 package com.beon.androidchallenge.ui.main
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -36,15 +38,35 @@ class MainFragment : Fragment() {
         initObservers()
     }
 
+    private fun toggleProgressBar(isVisible: Boolean) {
+        with(binding) {
+            progressBar.isVisible = isVisible
+            factTextView.isVisible = !isVisible
+        }
+    }
+
+    private val handler = Handler()
+
     private fun initViews() {
         binding.run {
+            toggleProgressBar(isVisible = false)
+
             numberEditText.addTextChangedListener {
-                viewModel.searchNumberFact(it.toString())
+                if(it.toString().isNotEmpty()) {
+                    toggleProgressBar(isVisible = true)
+                }
+
+                val h = handler.postDelayed(Runnable {
+                    viewModel.searchNumberFact(it.toString())
+
+                }, 1000)
+
             }
 
             numberEditText.setOnEditorActionListener { textView, actionId, keyEvent ->
                 if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                     viewModel.searchNumberFact(textView.text.toString())
+                    toggleProgressBar(isVisible = true)
                     return@setOnEditorActionListener true
                 } else {
                     return@setOnEditorActionListener false
@@ -53,6 +75,7 @@ class MainFragment : Fragment() {
 
             retryButton.setOnClickListener {
                 viewModel.searchNumberFact(numberEditText.text.toString())
+                toggleProgressBar(isVisible = true)
             }
 
             numberEditText.requestFocus()
@@ -60,12 +83,18 @@ class MainFragment : Fragment() {
     }
 
     private fun initObservers() {
-        viewModel.currentFact.observeForever {
+        viewModel.currentFact.observe(viewLifecycleOwner) {
             if (binding.numberEditText.text.toString().isEmpty()) {
                 binding.factTextView.setText(R.string.instructions)
             } else {
+                toggleProgressBar(isVisible = false)
                 binding.factTextView.text = it?.text
             }
+        }
+
+        viewModel.error.observe(viewLifecycleOwner) {
+            toggleProgressBar(isVisible = false)
+            binding.factTextView.text = it
         }
     }
 
--- app/src/main/java/com/beon/androidchallenge/ui/main/MainViewModel.kt
+++ app/src/main/java/com/beon/androidchallenge/ui/main/MainViewModel.kt
@@ -4,26 +4,46 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.beon.androidchallenge.data.repository.FactRepository
 import com.beon.androidchallenge.domain.model.Fact
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
     val currentFact = MutableLiveData<Fact?>(null)
+    val error = MutableLiveData<String>(null)
+
+    private val viewModelScope = CoroutineScope(Dispatchers.Default)
+
+    private var lastCallTime = 0L
+    private val callInterval = 2000L
 
     fun searchNumberFact(number: String) {
+
         if (number.isEmpty()) {
             currentFact.postValue(null)
             return
         }
 
-        FactRepository.getInstance().getFactForNumber(number, object : FactRepository.FactRepositoryCallback<Fact> {
-            override fun onResponse(response: Fact) {
-                currentFact.postValue(response)
-            }
+        val currentTime = System.currentTimeMillis()
 
-            override fun onError() {
-                
-            }
+        if (currentTime - lastCallTime >= callInterval) {
+            lastCallTime = currentTime
 
-        })
+            viewModelScope.launch {
+
+                FactRepository.getInstance()
+                    .getFactForNumber(number, object : FactRepository.FactRepositoryCallback<Fact> {
+                        override fun onResponse(response: Fact) {
+                            currentFact.postValue(response)
+                        }
+
+                        override fun onError() {
+                            error.postValue("API ERROR. Try again")
+                        }
+
+                    })
+            }
+        }
     }
 }
